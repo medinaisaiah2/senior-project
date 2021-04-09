9bd8c6dfad86ee530658dfd5228f7b6cfd25d2d0
alex <alex@localhost>
2021-04-09 01:28:34 -0700

    changed how we download data

--- scripts/download_appendrsi.py
+++ scripts/download_appendrsi.py
@@ -2,7 +2,8 @@ import datetime
 import pandas_datareader.data as web
 from cal_rsi import calculatersi
 def download_n_save(stock):
-    today = datetime.datetime(2021, 2, 18)  # make it same day as the others which is 12/08/2020
+    #today = datetime.datetime(2021, 2, 18)  # make it same day as the others which is 12/08/2020
+    today = datetime.datetime.now()
     endd = datetime.datetime(today.year, today.month, today.day - 1)  # yesterday
     startd = datetime.datetime(today.year - 1, today.month, today.day - 1)
 
