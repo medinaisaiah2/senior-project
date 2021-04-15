a920b1649190691ebdcd138623288f795b8f5e4d
alex <alex@localhost>
2021-04-15 15:49:29 -0700

    added a template of myconfig

---
+++ scripts/myconfig.py
@@ -0,0 +1,7 @@
+APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
+
+APCA_DATA_BASE_URL = "https://data.alpaca.markets"
+
+API_KEY_ID = "12345678901234567890"
+
+API_SECRET_KEY = "somesecretkeywithcharactersandnumbersaboutthisize"
